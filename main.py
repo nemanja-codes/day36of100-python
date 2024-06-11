6a9938e6aeb97c9da4d0b9ba448c70f445caf059
Nemanja Markovic <necamark@gmail.com>
2024-06-11 22:07:14 +0200

    "get first 3 articles related to company name using News API"

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ import requests
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-API_KEY = "KY2MZI2P1U1GSU31"
+STOCK_API_KEY = "KY2MZI2P1U1GSU31"
+NEWS_API_KEY = "9aec1dd0fd6d477c9459e2327937d753"
 
-parameters = {
+stock_parameters = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
-    "apikey": API_KEY,
+    "apikey": STOCK_API_KEY,
+}
+
+news_parameters = {
+    "qInTitle": COMPANY_NAME,
+    "apiKey": NEWS_API_KEY
 }
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-response = requests.get(url="https://www.alphavantage.co/query", params=parameters)
+response = requests.get(url="https://www.alphavantage.co/query", params=stock_parameters)
 response.raise_for_status()
 stock_data = response.json()
 daily_data = stock_data["Time Series (Daily)"]
@@ -20,11 +26,17 @@ days_to_compare = list(daily_data.items())[:2]
 yesterday = days_to_compare[0][1]
 day_before_yesterday = days_to_compare[1][1]
 diff = float(yesterday["4. close"]) - float(day_before_yesterday["4. close"])
+
 if diff < 0:
     diff *= -1
+
 diff_in_percents = (diff * 100) / float(yesterday["4. close"])
-if diff_in_percents > 5:
-    print("Get News")
+if diff_in_percents > 1:
+    news_response = requests.get(url=" https://newsapi.org/v2/everything", params=news_parameters)
+    news_response.raise_for_status()
+    news_data = news_response.json()
+    first_three_articles = news_data["articles"][0:3]
+
 
 
 ## STEP 2: Use https://newsapi.org
